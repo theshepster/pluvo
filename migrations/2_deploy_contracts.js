0288a64d688938f73b835e28b57d3de2da96fb0f
Shep Moore-Berg <mooreberg@g.harvard.edu>
2018-07-18 22:47:20 -0400

    Update 2_deploy_contracts.js migration file with default parameters.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,17 @@
 var Pluvo = artifacts.require("./Pluvo.sol");
 
+// TODO: UPDATE THESE PARAMETERS BEFORE LIVENET DEPLOYMENT
+const maxSupply = 100;
+const evaporationRate = 1;
+const evaporationDenominator = 4;
+const blocksBetweenRainfalls = 1;
+
 module.exports = function(deployer) {
-  deployer.deploy(Pluvo);
+  deployer.deploy(
+    Pluvo, 
+    maxSupply, 
+    evaporationRate, 
+    evaporationDenominator, 
+    blocksBetweenRainfalls
+  );
 };
