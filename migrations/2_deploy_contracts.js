f011b2c9e9a5912bc68da1a95ddb6b4d8972e1cf
Shep Moore-Berg <theshepster@gmail.com>
2018-07-31 18:56:33 -0400

    Add drizzle box files

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,4 +1,7 @@
 var Pluvo = artifacts.require("./Pluvo.sol");
+var SimpleStorage = artifacts.require("SimpleStorage"); // TODO: REMOVE
+var TutorialToken = artifacts.require("TutorialToken"); // TODO: REMOVE
+var ComplexStorage = artifacts.require("ComplexStorage"); // TODO: REMOVE
 
 // TODO: UPDATE THESE PARAMETERS BEFORE LIVENET DEPLOYMENT
 const maxSupply = 100;
@@ -7,6 +10,9 @@ const evaporationDenominator = 4;
 const blocksBetweenRainfalls = 1;
 
 module.exports = function(deployer) {
+  deployer.deploy(SimpleStorage); // TODO: REMOVE
+  deployer.deploy(TutorialToken); // TODO: REMOVE
+  deployer.deploy(ComplexStorage); // TODO: REMOVE
   deployer.deploy(
     Pluvo, 
     maxSupply, 
--- truffle.js
+++ truffle.js
@@ -13,14 +13,21 @@
  */
 
 /* 
- * Use Ganache
+ * Use Ganache-CLI
  */
 module.exports = {
+  migrations_directory: "./migrations",
   networks: {
     development: {
       host: "127.0.0.1",
-      port: 7545,
-      network_id: "*"
+      port: 8545,
+      network_id: "*" // Match any network id
     }
-  }
+  },
+  solc: {
+    optimizer: {
+      enabled: true,
+      runs: 500
+    }
+  } 
 };
